ee473e28edc99af26b5c7cdcada818cf14e95796
ju <julienurraca@gmail.com>
2020-07-18 18:39:33 -0500

    README, format

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# SubstackToGhost
+## SubstackToGhost
 
 The script takes in an export folder from Substack, and generates a Ghost-JSON compliant export, ready to import into Ghost. 
+Requires Erlang installed. 
 For now, just slams html into mobiledoc cards. 
-WIP. 
+WIP. Todo: 
+    - better option parsing
+    - standalone executable? 
+    - test cards vs other mobiledoc options
 
--- lib/cli.ex
+++ lib/cli.ex
@@ -1,17 +1,17 @@
 defmodule SubstackToGhost.CLI do
-    @moduledoc """
-    CLI Parser
-    """
+  @moduledoc """
+  CLI Parser
+  """
 
-    def main(args) do
-        args
-        |> parse_args()
-        |> run()
-    end
+  def main(args) do
+    args
+    |> parse_args()
+    |> run()
+  end
 
-    def parse_args([arg | _]) when is_binary(arg) do
-      arg
-    end
+  def parse_args([arg | _]) when is_binary(arg) do
+    arg
+  end
 
-    def run(path), do: SubstackToGhost.build_ghost_json(path)
+  def run(path), do: SubstackToGhost.build_ghost_json(path)
 end
