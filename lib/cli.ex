9c895cf7e83702720d34a990c93c1c2ade938277
ju <julienurraca@gmail.com>
2020-07-18 18:25:25 -0500

    add cli

---
+++ lib/cli.ex
@@ -0,0 +1,17 @@
+defmodule SubstackToGhost.CLI do
+    @moduledoc """
+    CLI Parser
+    """
+
+    def main(args) do
+        args
+        |> parse_args()
+        |> run()
+    end
+
+    def parse_args([arg | _]) when is_binary(arg) do
+      arg
+    end
+
+    def run(path), do: SubstackToGhost.build_ghost_json(path)
+end
